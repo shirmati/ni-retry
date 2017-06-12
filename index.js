fdc6d0c1d3a81d1690de2130469cd0906b713bb3
Zach Erskine <zachary.erskine@naturalint.com>
2017-06-12 11:21:50 +0300

    more work

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 'use strict';
 
-module.exports = function(db, serviceName) {
+module.exports = function(app,db, serviceName) {
+  let instance;
 
-  return {
-    middleware: {
-      retryMiddleware: require('./lib/middleware/retry-middleware')(db,serviceName),
-      auditLogMiddleware: require('./lib/middleware/audit-log-middleware')(db,serviceName),
-    },
-    registerRealtimeSchemas : require('./lib/helpers/dbInit')(db)
+  if (!instance){
+    instance = {
+      middleware: {
+        retryMiddleware: require('./lib/middleware/retry-middleware')(app,db,serviceName),
+        auditLogMiddleware: require('./lib/middleware/audit-log-middleware')(db,serviceName),
+      },
+      helpers: {
+        dbHelper: require('./lib/helpers/dbHelper')(db,serviceName)
+      },
+      registerRealtimeSchemas : require('./lib/helpers/dbInit')(db)
+    }
   }
+  return instance;
 };
